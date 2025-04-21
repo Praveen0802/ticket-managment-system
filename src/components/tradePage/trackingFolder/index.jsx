a2c8b55829b1695a6f689289b9fd0439f7e22797
praveen <praveenkanth.k@vakilsearch.com>
2025-04-21 19:57:32 +0530

    main

--- src/components/tradePage/trackingFolder/index.jsx
+++ src/components/tradePage/trackingFolder/index.jsx
@@ -23,6 +23,7 @@ import {
 } from "@/utils/apiHandler/request";
 import { updateConfirmPurchasePopup } from "@/utils/redux/common/action";
 import { useDispatch } from "react-redux";
+import useIsMobile from "@/utils/helperFunctions/useIsmobile";
 
 const TrackingPage = (props) => {
   const { response = {} } = props;
@@ -39,6 +40,7 @@ const TrackingPage = (props) => {
   });
   const [loader, setLoader] = useState(false);
   const dispatch = useDispatch();
+  const isMobile = useIsMobile();
   const [filterApplied, setFIlterApplied] = useState({});
   const headers = [
     { key: "status", label: "Status", sortable: true },
@@ -48,8 +50,15 @@ const TrackingPage = (props) => {
     { key: "category", label: "Category", sortable: true },
     { key: "ticketType", label: "Ticket Type", sortable: true },
     { key: "qty", label: "Qty", sortable: true },
+    ...(isMobile
+      ? [
+          { key: "price", label: "Price", sortable: true },
+          { key: "attachment", label: "" },
+          { key: "hand", label: "" },
+          { key: "document", label: "" },
+        ]
+      : []),
   ];
-
   const trackingFetchApi = async (params, availablityStatus) => {
     setLoader(true);
     const response = await purchaseTracking("", "GET", params);
@@ -90,6 +99,31 @@ const TrackingPage = (props) => {
       category: item?.Category,
       ticketType: item?.ticket_type,
       qty: item?.quantity,
+      ...(isMobile && {
+        price: item?.price_with_currency,
+        attachment: (
+          <Image
+            width={14}
+            height={14}
+            src={
+              item?.ticket_type_id == 2
+                ? attachmentPin
+                : item?.ticket_type_id == 4 || item?.ticket_type_id == 6
+                ? attachment6
+                : item?.ticket_type_id == 3
+                ? attachment3
+                : item?.ticket_type_id == 1
+                ? attachment1
+                : attachmentPin
+            }
+            alt="attach"
+          />
+        ),
+        hand: <Image width={16} height={16} src={crossHand} alt="hand" />,
+        document: item?.listing_note?.length > 0 && (
+          <Image width={20} height={20} src={documentText} alt="document" />
+        ),
+      }),
     };
   });
 
@@ -119,74 +153,84 @@ const TrackingPage = (props) => {
     );
   };
 
-  const rightStickyHeaders = ["Ticket Price"];
-
+  const rightStickyHeaders = isMobile ? [] : ["Ticket Price"];
   const rightStickyColumns = selectedTicketDetails?.map((item) => {
     return [
-      {
-        icon: <p>{item?.price_with_currency}</p>,
-        className: "border-r-[1px] border-[#E0E1EA] text-[#323A70] text-[12px]",
-      },
-      {
-        icon: (
-          <Image
-            width={14}
-            height={14}
-            src={
-              item?.ticket_type_id == 2
-                ? attachmentPin
-                : item?.ticket_type_id == 4 || item?.ticket_type_id == 6
-                ? attachment6
-                : item?.ticket_type_id == 3
-                ? attachment3
-                : item?.ticket_type_id == 1
-                ? attachment1
-                : attachmentPin
-            }
-            alt="attach"
-          />
-        ),
-        className: "cursor-pointer pl-2",
-        key: "attach",
-      },
-      {
-        icon: <Image width={16} height={16} src={crossHand} alt="hand" />,
-        className: "cursor-pointer px-2",
-        key: "oneHand",
-        tooltipComponent: (
-          <p className="text-center">
-            Expected Delivery Date:
-            <br />
-            {dateFormat(item?.expected_date_inhand)}
-          </p>
-        ),
-        tooltipPosition: "top",
-      },
-      {
-        icon: item?.listing_note?.length > 0 && (
-          <Image width={20} height={20} src={documentText} alt="document" />
-        ),
-        className: "cursor-pointer pr-2",
-        key: "document",
-        tooltipComponent:
-          item?.listing_note?.length > 0 &&
-          item?.listing_note?.map((note, index) => (
-            <div className="flex flex-col gap-2">
-              <p className="text-left">Benifits/Restrictions</p>
-              <ul
-                className={`list-disc ml-[20px] ${
-                  item?.listing_note?.length > 3 && "grid grid-cols-2 gap-1"
-                }`}
-                key={index}
-              >
-                {Object.values(note).map((value, i) => (
-                  <li key={i}>{value}</li>
-                ))}
-              </ul>
-            </div>
-          )),
-        tooltipPosition: "top",
-      },
+      ...(isMobile
+        ? []
+        : [
+            {
+              icon: <p>{item?.price_with_currency}</p>,
+              className:
+                "border-r-[1px] border-[#E0E1EA] text-[#323A70] text-[12px]",
+            },
+            {
+              icon: (
+                <Image
+                  width={14}
+                  height={14}
+                  src={
+                    item?.ticket_type_id == 2
+                      ? attachmentPin
+                      : item?.ticket_type_id == 4 || item?.ticket_type_id == 6
+                      ? attachment6
+                      : item?.ticket_type_id == 3
+                      ? attachment3
+                      : item?.ticket_type_id == 1
+                      ? attachment1
+                      : attachmentPin
+                  }
+                  alt="attach"
+                />
+              ),
+              className: "cursor-pointer pl-2",
+              key: "attach",
+            },
+            {
+              icon: <Image width={16} height={16} src={crossHand} alt="hand" />,
+              className: "cursor-pointer px-2",
+              key: "oneHand",
+              tooltipComponent: (
+                <p className="text-center">
+                  Expected Delivery Date:
+                  <br />
+                  {dateFormat(item?.expected_date_inhand)}
+                </p>
+              ),
+              tooltipPosition: "top",
+            },
+            {
+              icon: item?.listing_note?.length > 0 && (
+                <Image
+                  width={20}
+                  height={20}
+                  src={documentText}
+                  alt="document"
+                />
+              ),
+              className: "cursor-pointer pr-2",
+              key: "document",
+              tooltipComponent:
+                item?.listing_note?.length > 0 &&
+                item?.listing_note?.map((note, index) => (
+                  <div className="flex flex-col gap-2" key={index}>
+                    <p className="text-left">Benifits/Restrictions</p>
+                    <ul
+                      className={`list-disc ml-5 ${
+                        item?.listing_note?.length > 3
+                          ? "grid grid-cols-2 gap-1"
+                          : ""
+                      }`}
+                    >
+                      {Object.values(note).map((value, i) => (
+                        <li key={i}>{value}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )),
+              tooltipPosition: "top",
+            },
+          ]),
       {
         icon: (
           <Image
@@ -266,7 +310,7 @@ const TrackingPage = (props) => {
     <div className="flex flex-col gap-[24px]">
       <div className="bg-white">
         <div className="py-[20px] px-[24px] border-b-[1px] border-[#E0E1EA]">
-          <div className=" flex gap-4 w-[70%]">
+          <div className=" flex max-md:flex-col gap-4 md:w-[70%]">
             {items.map((item, index) => (
               <AvailableList
                 key={index}
@@ -285,7 +329,7 @@ const TrackingPage = (props) => {
           </div>
         </div>
         <div className="py-[20px] px-[24px] border-b-[1px] border-[#E0E1EA]">
-          <div className="flex gap-4 items-center w-[50%]">
+          <div className="flex max-md:flex-col gap-4 items-center w-[50%]">
             <FloatingLabelInput
               id="selectedMatch"
               name="selectedMatch"
@@ -301,7 +345,7 @@ const TrackingPage = (props) => {
               id="eventDate"
               name="eventDate"
               keyValue="eventDate"
-              parentClassName="!w-[350px]"
+              parentClassName="!md:w-[350px] !max-md:w-full"
               label="Event Date"
               className="!py-[8px] !px-[16px] mobile:text-xs"
               value={selectedDate}
